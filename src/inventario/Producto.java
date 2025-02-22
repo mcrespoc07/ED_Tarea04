04c3e3905814a04827c2daec14f73c0781f2dd16
reme <reme@reme-vostro3500>
2025-02-22 20:23:11 +0100

    Versión inicial sin modificar Signed-off-by:reme <reme@reme-vostro3500>

--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -1,9 +1,9 @@
 package inventario;
 
 public class Producto {
-    public String n;
-    public double p;
-    public int s;
+    private String n;
+    private double p;
+    private int s;
 
     public Producto(String nombre, double precio, int stock) {
         this.n = nombre;
@@ -20,4 +20,29 @@ public class Producto {
         p = 0.0;
         s = 0;
     }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public double getP() {
+        return p;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+    
 }
